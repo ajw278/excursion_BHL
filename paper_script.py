ec8ffe8e0305a33f55cd35a9d1048bf7af8c189f
Andrew Winter <andrew@MacBook-Pro-6.local>
2024-07-17 14:15:23 +0200

    print tags

--- paper_script.py
+++ paper_script.py
@@ -62,15 +62,6 @@ if __name__=='__main__':
 	sfdb.plot_vturb(tag=tag)
 	sfdb.plot_Next(tag=tag)
 
-	print('\n\n *******IC disc vary, no wind *******\n\n')
-	
-
-	#Test results for a substantial initial disc mass
-	tag = sfdb.calc_discevol(redraw=False, Nsample=500, minit=0.01, minitdisp=1.0, ptag='m250_', wind=False, eps_wind=0.1)
-
-	sfdb.plot_accretion_rates_wevap(tag=tag)
-	sfdb.plot_all(tag=tag)
-	sfdb.plot_rplf(tag=tag)
 
 
 	print('\n\n *******Wind, 0 initial disc mass*******\n\n')
@@ -78,6 +69,16 @@ if __name__=='__main__':
 
 	#Test results for a substantial initial disc mass
 	tag = sfdb.calc_discevol(redraw=False, Nsample=500, minit=0.0, minitdisp=0.0, ptag='m250_', wind=True, eps_wind=0.1)
+
+	sfdb.plot_accretion_rates_wevap(tag=tag)
+	sfdb.plot_all(tag=tag)
+	sfdb.plot_rplf(tag=tag)
+
+	print('\n\n *******IC disc vary, no wind *******\n\n')
+	
+
+	#Test results for a substantial initial disc mass
+	tag = sfdb.calc_discevol(redraw=False, Nsample=500, minit=0.01, minitdisp=1.0, ptag='m250_', wind=False, eps_wind=0.1)
 
 	sfdb.plot_accretion_rates_wevap(tag=tag)
 	sfdb.plot_all(tag=tag)
