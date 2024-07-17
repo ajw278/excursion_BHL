ae4e20af3e66c345a06041a555f3ce0a352e250c
Andrew Winter <andrew@MacBook-Pro-6.local>
2024-07-17 14:42:54 +0200

    mdisp = 0

--- paper_script.py
+++ paper_script.py
@@ -78,7 +78,7 @@ if __name__=='__main__':
 	
 
 	#Test results for a substantial initial disc mass
-	tag = sfdb.calc_discevol(redraw=False, Nsample=500, minit=0.01, minitdisp=1.0, ptag='m250_', wind=False, eps_wind=0.1)
+	tag = sfdb.calc_discevol(redraw=False, Nsample=500, minit=0.01, minitdisp=0.0, ptag='m250_', wind=False, eps_wind=0.1)
 
 	sfdb.plot_accretion_rates_wevap(tag=tag)
 	sfdb.plot_all(tag=tag)
