94727529f0c366754449fab1dd68e9e2e6a91cc0
Andrew Winter <andrew@MacBook-Pro-6.local>
2024-07-17 13:07:42 +0200

    wind and ics correct paper script

--- paper_script.py
+++ paper_script.py
@@ -9,6 +9,29 @@ if __name__=='__main__':
 	#exit()
 	#
 
+	#Genererate density histories for at least 10000 stars in at least 500 different regions
+	sfdb = excursion.get_density_history(GMCmin=2.0, GMCmax=1000.0, Nregions=1000, Nstars=10000, tag='_ll1')
+
+	#wl.construct_grid()
+
+	#For a representative sample of 500 stars, calculate the disc evolution
+	tag = sfdb.calc_discevol(redraw=False, Nsample=500, minit=0.0, minitdisp=0.0, mlim=250., ptag='m250_'+sfdb.tag+'_', wind=False, eps_wind=0.1)
+	print('Tag:', tag)
+	
+	#Plot the BHL accretion rate histories
+	sfdb.plot_accretion_rates_wevap(tag=tag)
+
+	sfdb.plot_discfrac_msplit(tag=tag)
+	#Plot the BHL accretion rate histories
+	#sfdb.plot_accretion_rates()
+
+	#Make the plots for the paper
+	sfdb.plot_discfrac(tag=tag)
+	sfdb.plot_rplf(tag=tag)
+	sfdb.plot_all(tag=tag)
+	sfdb.plot_vturb(tag=tag)
+	sfdb.plot_Next(tag=tag)
+
 
 	#Genererate density histories for at least 10000 stars in at least 500 different regions
 	sfdb = excursion.get_density_history(GMCmin=10.0, GMCmax=1000.0, Nregions=300, Nstars=5000, tag='')
@@ -34,34 +57,20 @@ if __name__=='__main__':
 	sfdb.plot_vturb(tag=tag)
 	sfdb.plot_Next(tag=tag)
 
-	#Genererate density histories for at least 10000 stars in at least 500 different regions
-	sfdb = excursion.get_density_history(GMCmin=2.0, GMCmax=1000.0, Nregions=1000, Nstars=10000, tag='_ll1')
-
-	#wl.construct_grid()
-
-	#For a representative sample of 500 stars, calculate the disc evolution
-	tag = sfdb.calc_discevol(redraw=False, Nsample=500, minit=0.0, minitdisp=0.0, mlim=250., ptag='m250_'+sfdb.tag+'_', wind=False, eps_wind=0.1)
-	print('Tag:', tag)
 	
-	#Plot the BHL accretion rate histories
+
+	#Test results for a substantial initial disc mass
+	tag = sfdb.calc_discevol(redraw=False, Nsample=500, minit=0.01, minitdisp=1.0, ptag='m250_', wind=False, eps_wind=0.1)
+
 	sfdb.plot_accretion_rates_wevap(tag=tag)
-
-	sfdb.plot_discfrac_msplit(tag=tag)
-	#Plot the BHL accretion rate histories
-	#sfdb.plot_accretion_rates()
-
-	#Make the plots for the paper
-	sfdb.plot_discfrac(tag=tag)
-	sfdb.plot_rplf(tag=tag)
 	sfdb.plot_all(tag=tag)
-	sfdb.plot_vturb(tag=tag)
-	sfdb.plot_Next(tag=tag)
+	sfdb.plot_rplf(tag=tag)
+
 
 
 	#Test results for a substantial initial disc mass
-	tag = sfdb.calc_discevol(redraw=False, Nsample=500, minit=0.01, minitdisp=1.0, ptag='m250_', wind=True, eps_wind=0.1)
+	tag = sfdb.calc_discevol(redraw=False, Nsample=500, minit=0.0, minitdisp=0.0, ptag='m250_', wind=True, eps_wind=0.1)
 
 	sfdb.plot_accretion_rates_wevap(tag=tag)
 	sfdb.plot_all(tag=tag)
 	sfdb.plot_rplf(tag=tag)
-
